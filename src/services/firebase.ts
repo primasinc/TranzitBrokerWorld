5094c08134daac3aac2d6a5808d68ee509a6cfe1
Kardiackid419 <kevinchrv1@gmail.com>
2025-04-13 21:37:03 -0400

    Update Firebase configuration

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,3 +1,8 @@
 import app, { storage, auth, db } from '../config/firebase';
 
-export { app as firebaseApp, storage as firebaseStorage, auth as firebaseAuth, db as firebaseDb }; 
+export { 
+  app as firebaseApp, 
+  storage as firebaseStorage, 
+  auth as firebaseAuth,
+  db as firebaseDb 
+}; 
